cd5384ef9b3e809f921706b7f72cc1980ba82487
PS <pseo002@gmail.com>
2013-03-30 18:43:33 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Balls::Application.config.secret_token = '3089fbda0264fe8c4767f32f32949ca7eee60dd7bda604945ba25eef92fc71013b2003548f4e269131eeb78aabb2bfddcda9ad97e2052a49cf3ef699fccc14c6'
